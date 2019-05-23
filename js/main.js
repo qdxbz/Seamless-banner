12117f5ff941d40a35ee4e5f01a948c2041d895c
mygod <13683244634@163.com>
2019-05-23 13:26:39 +0800

    banner

--- js/main.js
+++ js/main.js
@@ -1,65 +1,54 @@
-$('.images > img:nth-child(1)').addClass('current');
-$('.images > img:nth-child(2)').addClass('enter');
-$('.images > img:nth-child(3)').addClass('enter');
-$('.images > img:nth-child(4)').addClass('enter');
-let n=1;
-setInterval(()=>{
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-    .one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
-    })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current');
-    n+=1
-},3000)
-function x(n){
-    if(n>4){
-        n = n%4
-        if(n===0){
-            n=4
-        }
-    }//n= 1 2 3 4 
-    return n
-}
+var $li=$(".banner_img").children();
+var $sli=$(".slider_nav").children();
+setInterval(function(){
+  $(".banner").css("height",$li.height());
+},100);
 
-/* 优化版
-let n
-初始化()
-setInterval(()=>{
-  makeLeave(getImage(n))
-    .one('transitionend', (e)=>{
-      makeEnter($(e.currentTarget))
-    })
-  makeCurrent(getImage(n+1))
-  n += 1
-},3000)
-
-function getImage(n){
-  return $(`.images > img:nth-child(${x(n)})`)
-}
-
-function x(n){
-  if(n>3){
-    n = n%3
-    if (n===0){
-      n =3
+$(".slider-page").on("click","a",function(e){
+  e.preventDefault();
+  var $a=$(this);
+  var index;
+  $.each($li,function(i,elem){
+    if($(elem).is(".banner_active")){
+      index=parseInt($(elem).index());
     }
-  } // n = 1 2 3
-  return n
-}
-
-function 初始化(){
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-
-function makeCurrent($node){
-  return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-  return $node.removeClass('enter current').addClass('leave')
-}
-function makeEnter($node){
-  return $node.removeClass('leave current').addClass('enter')
-}
-*/
+  })
+  if($a.is(".prevNav")){
+    if(index>0){
+      $($li[index]).removeClass().prev().addClass("banner_active");
+      $($sli[index-1]).addClass("slider_active").siblings().removeClass();
+    }else{
+      $($li[index]).removeClass();
+      $($li[$li.length-1]).addClass("banner_active");
+      $($sli[$sli.length-1]).addClass("slider_active").siblings().removeClass();
+    }
+  }else{
+    if(index<3){
+      $($li[index]).removeClass().next().addClass("banner_active");
+      $($sli[index+1]).addClass("slider_active").siblings().removeClass();
+    }else{
+      $($li[index]).removeClass();
+      $($li[0]).addClass("banner_active");
+      $($sli[0]).addClass("slider_active").siblings().removeClass();
+    }
+  }
+})
+timer=setInterval(function(){
+  $(".nextNav").click();
+},2000);
+// 自动轮播
+$(".banner").mouseenter(function(){
+  clearInterval(timer);
+  timer=null;
+}).mouseleave(function(){
+  timer=setInterval(function(){
+    $(".nextNav").click();
+  },2000);
+})
+// 悬浮轮播
+$(".slider_nav").on("mouseover","li",function(){
+  var $sli=$(this);
+  var index=$sli.index();
+  $sli.addClass("slider_active").siblings().removeClass();
+  $($li[index]).addClass("banner_active").siblings().removeClass();
+})
